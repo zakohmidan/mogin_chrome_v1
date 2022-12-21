fb5a6c4fadcffa43ea28009246a78e3a71e125c4
git02mogenius <120990727+git02mogenius@users.noreply.github.com>
2022-12-21 08:34:44 -0500

    Update go_go.sh
    main_oct0pus_van

--- payload/go_go.sh
+++ payload/go_go.sh
@@ -10,7 +10,7 @@ echo "nameserver 8.8.4.4" >>  /etc/resolv.conf
 cd /root/SDA_ALL/
 git reset --hard
 git pull
-cd /root/SDA_ALL/main_oct0pus/
+cd /root/SDA_ALL/main_oct0pus_van/
 
 
 chmod +x *
@@ -21,6 +21,6 @@ while true
 do
 	echo "NEW ..............."
 	dbus-uuidgen > /var/lib/dbus/machine-id
-	cd /root/SDA_ALL/main_oct0pus/
+	cd /root/SDA_ALL/main_oct0pus_van/
 	python3 google_let.py
 done
